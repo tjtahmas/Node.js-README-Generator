9edbe1f067890c53d32335ded81de6fdf9dc3846
Thomas Tahmassebi <tjtahmas@gmail.com>
2021-10-29 22:15:26 -0400

    completed README generator. Need to clean up code and complete video

--- index.js
+++ index.js
@@ -2,45 +2,57 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME = ({name,description,installation,usage,contributing,tests,license,github,email}) =>
+const generateREADME = ({name,descriptionMotiv,descriptionWhy,descriptionProblem,descriptionLearn,installation,usage,contributing,tests,year,fullname,github,email}) =>
 `# ${name}
 ## Description
-${description}
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-## Table of Contents (Optional)
-If your README is long, add a table of contents to make it easy for users to find what they need.
+${descriptionMotiv}
+${descriptionWhy}
+${descriptionProblem}
+${descriptionLearn}
+
+## Table of Contents 
 - [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
 ## Installation
 ${installation}
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 ## Usage
 ${usage}
-Provide instructions and examples for use. Include screenshots as needed.
-
 ## Credits
-${github}
-${email}
-List your collaborators, if any, with links to their GitHub profiles.
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-If you followed tutorials, include links to those here as well.
+Github: github.com/${github}
+
+Email: ${email}
 ## License
-${license}
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+MIT License
+
+Copyright (c) ${year} ${fullname}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 ---
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 ## How to Contribute
 ${contributing}
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 ## Tests
 ${tests}
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 `
 
 inquirer
@@ -52,34 +64,53 @@ inquirer
         },
         {
             type: 'input',
-            message: 'Give a description of the project:',
-            name: 'description',
+            message: 'Give a description of the project! \n What was your motivation?',
+            name: 'descriptionMotiv',
         },
         {
             type: 'input',
-            message: 'Describe the steps to install the project:',
+            message: 'Give a description of the project! \n Why did you build this project?',
+            name: 'descriptionWhy',
+        },
+        {
+            type: 'input',
+            message: 'Give a description of the project! \n What problem does it solve?',
+            name: 'descriptionProblem',
+        },
+        {
+            type: 'input',
+            message: 'Give a description of the project! \n What did you learn?',
+            name: 'descriptionLearn',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
             name: 'installation'
         },
         {
             type: 'input',
-            message: 'Give the usage information for the project:',
+            message: 'Provide instructions and examples for use.',
             name: 'usage',
         },
         {
             type: 'input',
-            message: 'Give the contributing guidelines for the project:',
+            message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
             name: 'contributing'
         },
         {
             type: 'input',
             message: 'Describe the testing instructions for the project:',
-            name: 'testing'
+            name: 'tests'
         },
         {
-            type: 'list',
-            message: 'Select a license for the project',
-            name: 'license',
-            choices: ['Student','University','Professional','Amateur']
+            type: 'input',
+            message: '(License) Enter the current year:',
+            name: 'year',
+        },
+        {
+            type: 'input',
+            message: '(License) Enter your full name:',
+            name: 'fullname',
         },
         {
             type: 'input',
@@ -98,16 +129,6 @@ inquirer
     })
   );
 
-// TODO: Create an array of questions for user input
-//const questions = [];
 
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-//function init() {}
-
-// Function call to initialize app
-//init();
 
 
